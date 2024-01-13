e678ff79bcd2b6e0275d2a62c9744e84e7a27d49
fall <jfiejksu3@proton.me>
2024-01-14 01:24:28 +0800

    modified gitignore

--- basic.go
+++ basic.go
@@ -1,1 +1,12 @@
-package reLearn
+package main
+
+// so many sytax maybe i'll forget, so i just try 2 take down something
+
+func init() {
+	println("init everything, i just want to remember what i ever learned,first we have a few inits but only one main")
+}
+func main() {
+	println("^+num为按位取反", ^1)
+	println("有时导入一个包只需要执行它的init函数而不需要其中的内容,例如我们会使用 import _ \"./hello\",当然，一般我们认为一个dir就是一个package")
+
+}
